f2690fc477a5c0faeed2cffbf0a3c5b39b825470
tatarrr <tatarrr95@gmail.com>
2017-07-06 21:55:50 +1000

    осталось 2 блока

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($) {
+	$(".first-slider").owlCarousel({
+		items: 1,
+		nav: true,
+		loop: true,
+		dots: true
+	});
+	$(".second-slider").owlCarousel({
+		items: 1,
+		nav: true,
+		loop: true,
+		dots: true
+	});
+
+	ymaps.ready(function() {
+    var e = new ymaps.Map("Map", {
+            center: [43.101423, 131.94487],
+            zoom: 16
+        }),
+        i = new ymaps.control.TrafficControl;
+    e.controls.add(i), e.behaviors.disable("scrollZoom"), e.controls.remove("zoomControl"), placemark = new ymaps.Placemark([43.101734, 131.948654], {
+        iconContent: "",
+        iconContentSize: [300, 100]
+    }, {
+        preset: "islands#blueCircleIcon"
+    });
+    var s = [43.101735, 131.949185],
+        o = new ymaps.GeoObjectCollection({}, {
+            iconLayout: "default#image",
+            iconImageClipRect: [
+                [0, 0],
+                [33, 42]
+            ],
+            // iconImageHref: "img/map-marker1.png",
+            iconImageSize: [33, 42],
+            iconImageOffset: [-33, -42]
+        });
+    o.add(new ymaps.Placemark(s)), e.geoObjects.add(o)
+});
+});
